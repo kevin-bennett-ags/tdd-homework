bf81483a8a937fcfa614aef8f7a2e6ea899ac285
drelv <drel_v@yahoo.com>
2016-06-24 02:49:57 +0800

    6. String test case 2 numbers Pass

--- calculator.js
+++ calculator.js
@@ -17,7 +17,16 @@ exports.remainder = function(val1, val2) {
   return Number(val1) % Number(val2);
 }
 
-exports.addString = function(val) {		
-  return val==""? 0: Number(val);
+exports.addString = function(val) {
+	var qc = val.split(",");
+	var sum=0;
+	if(qc.length==2){
+		for(var i=0;i<qc.length;i++){
+			sum+=Number(qc[i]);			
+		}	
+	}else{
+		return sum==""? 0: Number(val);		
+	}		
+  	return sum;
 }
 
