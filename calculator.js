c15946dc0aa04fbae79acd9cfd8ac07906555b94
drelv <drel_v@yahoo.com>
2016-06-24 02:02:03 +0800

    6.Use TDD to create a new method addString - Test Pass for Method
    enter the commit message for your changes. Lines starting

--- calculator.js
+++ calculator.js
@@ -16,3 +16,8 @@ exports.multiply = function(val1, val2) {
 exports.remainder = function(val1, val2) {
   return Number(val1) % Number(val2);
 }
+
+exports.addString = function() {
+  return "";
+}
+
