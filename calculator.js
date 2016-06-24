1708b28a03de9ea5df761b341dea4c716e18751b
lagmanln <lagmanln@ph.ibm.com>
2016-06-24 15:07:12 +0800

    4. Make the remainder test pass (created remainder method)

--- calculator.js
+++ calculator.js
@@ -13,7 +13,7 @@ exports.multiply = function(val1, val2) {
 
 
 // bahavior for remainder
-/*exports.remainder = function(val1, val2) {
+exports.remainder = function(val1, val2) {
 	return Number(val1) % Number(val2);
-}*/
+}
 
