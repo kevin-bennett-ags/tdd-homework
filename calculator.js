9a0b63be982b6819aa885a0b9e99d8601a07eaf2
Hai Pham <haipham23@gmail.com>
2016-06-27 13:08:44 +0800

    add example for calculator methods and tests

--- calculator.js
+++ calculator.js
@@ -1,7 +1,15 @@
 'use strict';
 
-exports.add = function() {
-  // your code here.
+exports.add = function(strArr) {
+  var numArr = strArr.split(',').map(Number),
+      total = 0;
+
+  for(var i = 0, l = numArr.length; i < l; i++) {
+    if(numArr[i] < 0) throw new Error('negatives not allowed')
+    total += numArr[i];
+  }
+
+  return total;
 }
 
 exports.multiply = function(val1, val2) {
@@ -9,5 +17,19 @@ exports.multiply = function(val1, val2) {
 }
 
 // behavior for divide
+exports.divide = function(val1, val2) {
+  var num1 = Number(val1),
+      num2 = Number(val2);
+
+  if(num2 === 0) throw new Error('zero is not allowed as divider')
+  return num1 / num2;
+}
 
 // bahavior for remainder
+exports.remainder = function(val1, val2) {
+  var num1 = Number(val1),
+      num2 = Number(val2);
+
+  if(num2 === 0) throw new Error('zero is not allowed as divider')
+  return num1 % num2;
+}
